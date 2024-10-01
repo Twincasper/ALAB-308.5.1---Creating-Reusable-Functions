cff1d750745200674e2481652e2a131904ceec50
Justin Watson <61891187+Twincasper@users.noreply.github.com>
2024-10-01 10:09:02 -0400

    Completed part 3

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ const sumArray = (arr) => {
   return total;
 }
 
+console.log("sumArray: ",sumArray([1, 2, 3, 4, 5]));
+
 const avgArray = (arr) => {
   // We effectively just need to add up the array and divide by the length of the array.
   return sumArray(arr) / arr.length;
 }
+
+console.log("avgArray: ",avgArray([1, 2, 3, 4, 5]));
 
 const longestString = (arr) => {
   let longest = '';
@@ -22,6 +26,9 @@ const longestString = (arr) => {
   return longest;
 }
 
+console.log("longestString: ",longestString(['harley quinn', 'superman', 'lex luthor', 'the flash', 'batman', 'green lantern']));
+
+
 const stringsLongerThan = (arr, n) => {
   let longerStrings = [];
   for (let str of arr) {
@@ -32,41 +39,22 @@ const stringsLongerThan = (arr, n) => {
   return longerStrings;
 }
 
+console.log("stringsLongerThan: ",stringsLongerThan(['coffee', 'pepsi', 'tea', 'chai latte', 'slurpee'], 5));
+
 const printNumbers = (n) => {
-  let current = 1;
-  if (current > n) {
-    return;
-  } else {
-    console.log(current);
-    current++;
-    printNumbers(n);
+  // Need to print numbers from 1 to n inclusive, recursively
+  // The issue is this logs the numbers backwards, from n to 1. It says print every number between 1 and n, so technically this is right but still should look at it.
+  if (n > 0) {
+    console.log(n);
+    printNumbers(n - 1);
   }
 }
 
+printNumbers(10);
 
-// console.log(sumArray([1, 2, 3, 4, 5]));
-// console.log(avgArray([1, 2, 3, 4, 5]));
-// console.log(longestString(['harley quinn', 'superman', 'lex luthor', 'the flash', 'batman', 'green lantern']));
-// console.log(stringsLongerThan(['coffee', 'pepsi', 'tea', 'chai latte', 'slurpee'], 5));
-// printNumbers(10);
 
 // Part 2
 
-/*
-When functions are built into objects, like Arrays, they are referred to as “methods” of those objects. Many methods, including Array methods, require “callback functions” to determine their behavior.
-For the tasks below, use the following data to test your work:
-[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
- { id: "48", name: "Barry", occupation: "Runner", age: "25" },
- { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
- { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
- { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
-Use callback functions alongside Array methods to accomplish the following:
-Sort the array by age.
-Filter the array to remove entries with an age greater than 50.
-Map the array to change the “occupation” key to “job” and increment every age by 1.
-Use the reduce method to calculate the sum of the ages.
-Then use the result to calculate the average age.
-*/
 
 const data = [
   { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
@@ -92,6 +80,7 @@ console.log("Filtered Data:", filteredData);
 const occToJob = data.map(object => {
   // Need to change occupation key to job and increment age by 1, but keep age as a string somehow.
   object["job"] = object["occupation"];
+  delete object["occupation"];
   object["age"] = (parseInt(object["age"]) + 1).toString();
   return object;
 });
@@ -103,3 +92,38 @@ const AverageOfAges = data.reduce((accumulator, currentValue) => {
 }, 0) / data.length;
 
 console.log("Average Age:", AverageOfAges);
+
+// Part 3
+const batmanObj = { id: "42", name: "Bruce", occupation: "Knight", age: "41" };
+const damienObj = { id: "15", name: "Damien", occupation: "Son of Knight" };
+
+const incrementAge = (obj) => {
+  if (typeof obj['age'] === 'string') {
+  obj['age'] = (parseInt(obj['age']) + 1).toString();
+  } else if (!('age' in obj)) {
+    obj['age'] = "0";
+  } else {
+    obj['age']++;
+  }
+  obj['updated_at'] = new Date().toISOString();
+  return obj;
+}
+
+console.log("Dummy Object incrementAge:", incrementAge(batmanObj));
+
+const incrementCopysAge = (obj) => {
+  const copyObject = {...obj };
+
+  if (copyObject['age'] === undefined) {
+    copyObject['age'] = "0";
+  } else if (typeof copyObject['age'] ==='string') {
+    copyObject['age'] = (parseInt(copyObject['age']) + 1).toString();
+  } else {
+    copyObject['age']++;
+  }
+  copyObject['updated_at'] = new Date().toISOString();
+  return copyObject;
+}
+
+console.log("Dummy Object incrementCopysAge:", incrementCopysAge(damienObj));
+console.log("Batman object after incrementCopysAge:", incrementCopysAge(batmanObj));
